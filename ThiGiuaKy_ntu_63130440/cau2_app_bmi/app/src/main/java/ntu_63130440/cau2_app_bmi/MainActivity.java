b0a7d7b7aa990c1a524ef585b46a7411649c4355
DacHieu <checkerpssss@gmail.com>
2024-03-25 23:57:15 +0700

    upload 5

--- ThiGiuaKy_ntu_63130440/cau2_app_bmi/app/src/main/java/ntu_63130440/cau2_app_bmi/MainActivity.java
+++ ThiGiuaKy_ntu_63130440/cau2_app_bmi/app/src/main/java/ntu_63130440/cau2_app_bmi/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.graphics.Insets;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
 
 
 
